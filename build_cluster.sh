577f20760f51ffe71387b97345e990323bcbb6ba
Suraj Patil <srpatil@uci.edu>
2020-02-18 19:14:46 +0530

    Fix grammatical errors for daily commit

--- build_cluster.sh
+++ build_cluster.sh
@@ -3,8 +3,8 @@
 # Spark Configuration Variables (memory, instance, cores)
 
 # SPARK_WORKER_MEMORY: The System memory to be used by worker for creating executors on node. 
-# SPARK_MASTER_MEMORY: The memory for master Should not be too high.
-# SPARK_WORKER_CORES: The total number of cores to be used by executors by each worker
+# SPARK_MASTER_MEMORY: The memory for master should not be too high.
+# SPARK_WORKER_CORES: The total number of cores to be used by executors by each worker.
 # SPARK_WORKER_INSTANCES: The number of workers per worker node.
 
 NUM_OF_WORKERS=3
